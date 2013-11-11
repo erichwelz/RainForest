00834d042d31355f2e8955fd2df884c2e5d11e17
Erich Welz <erichwelz@gmail.com>
2013-11-11 16:32:12 -0500

    Adding user names

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def ensured_logged_in
+  def ensure_logged_in
     unless current_user
       flash[:alert] = "Please log in"
       redirect_to_new_session_path
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ActiveRecord::Base
   has_secure_password #automatically creates accessible :password, :password_confirmation
   has_many :reviews
   has_many :products, :through => :reviews
+  validates_presence_of :name
 end
